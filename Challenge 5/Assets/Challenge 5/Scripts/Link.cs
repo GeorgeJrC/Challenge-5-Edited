509d154892ca7d83062256d738bda417a4cb76af
GeorgeJrC <147717780+GeorgeJrC@users.noreply.github.com>
2023-12-22 04:07:54 +0000

    Reupload of Challenge 5

---
+++ Challenge 5/Assets/Challenge 5/Scripts/Link.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+    public void OpenGithub()
+    {
+        Application.OpenURL("https://github.com/GeorgeJrC/Challenge-5-Edited");
+    }
+}
